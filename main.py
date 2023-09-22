b6b40e5ef8fdbb9270d758a89874a9afef3fb36c
Neha Verma <nehbehl@gmail.com>
2023-09-22 17:03:43 +0530

    Adding fast api code and test for Render

---
+++ main.py
@@ -0,0 +1,31 @@
+from typing import Union, List
+from fastapi import FastAPI, HTTPException
+
+from pydantic import BaseModel
+
+
+class Data(BaseModel):
+    feature_1: float
+    feature_2: str
+
+
+app = FastAPI(
+    title="Exercise API",
+    description="An API that demonstrates checking the values of your inputs.",
+    version="1.0.0",
+)
+
+@app.get("/")
+async def say_hello():
+    return {"Udacity Machine Learning Devops"}
+
+@app.post("/data/")
+async def ingest_data(data: Data):
+    if data.feature_1 < 0:
+        raise HTTPException(status_code=400, detail="feature_1 needs to be above 0.")
+    if len(data.feature_2) > 280:
+        raise HTTPException(
+            status_code=400,
+            detail=f"feature_2 needs to be less than 281 characters. It has {len(data.feature_2)}.",
+        )
+    return data
