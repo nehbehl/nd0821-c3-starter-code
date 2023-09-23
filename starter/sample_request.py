5544c7e9ad4163fa9557fe37b19a4d6ec49d8ac6
Neha Verma <nehbehl@gmail.com>
2023-09-23 11:06:48 +0530

    Update sample_request.py

--- starter/sample_request.py
+++ starter/sample_request.py
@@ -1,8 +1,21 @@
 import requests
 import json
 
-data = {"name" : "Hitchhiking", "tags" : ["book1", "towel"], "item_id": 23}
+data = {
+        "age": 34,
+        "workclass": "Private",
+        "education": "Bachelors",
+        "education-num": 9,
+        "marital-status": "Married-civ-spouse",
+        "occupation": "Exec-managerial",
+        "relationship": "Wife",
+        "race": " White",
+        "sex": "Female",
+        "hours-per-week": "55",
+        "native-country": " United-States",
+    } 
 
-r = requests.post("http://127.0.0.1:8000/items/", data = json.dumps(data))
+r = requests.post("http://127.0.0.1:8000/predict/", data = json.dumps(data))
 
 print(r.json())
+
