eebdfc032fb956b93151e233380b1bea7ecec11a
Neha Verma <nehbehl@gmail.com>
2023-09-23 10:39:22 +0530

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -7,13 +7,48 @@ from main import app
 client = TestClient(app)
 
 
-def test_post_data_success():
-    data = {"age": 10, "workclass": "test string", "relationship" : "null"}
-    r = client.post("/data/", data=json.dumps(data))
-    assert r.status_code == 200
+
 
 
 def test_post_data_fail():
     data = {"age": 0, "workclass": "test string", "relationship" : "null"}
     r = client.post("/data/", data=json.dumps(data))
-    assert r.status_code == 400
+    assert r.status_code == 422
+
+def test_sal_less_50k():
+    data = {
+        "sno": 6,
+        "age": 49,
+        "workclass": "Private",
+        "education": "9th",
+        "education-num": 9,
+        "marital-status": "Married-spouse-absent",
+        "occupation": "Other-service",
+        "relationship": "Not-in-family",
+        "race": " Black",
+        "sex": "Female",
+        "hours-per-week": "16",
+        "native-country": "Jamaica"
+    } 
+    r = client.post("/predict/", json=data)
+    assert r.status_code == 200
+    assert r.json() == {"salary": "<=50k"}
+
+def test_sal_more_50k():
+    data = {
+        "sno": 7,
+        "age": 52,
+        "workclass": "Self-emp-not-inc",
+        "education": "HS-grad",
+        "education-num": 9,
+        "marital-status": "Married-civ-spouse",
+        "occupation": "Exec-managerial",
+        "relationship": "Husband",
+        "race": " White",
+        "sex": "Male",
+        "hours-per-week": "45",
+        "native-country": " United-States"
+    } 
+    r = client.post("/predict/", json=data)
+    assert r.status_code == 200
+    assert r.json() == {"salary": "<=50k"}
